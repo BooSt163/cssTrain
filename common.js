d4d49a9e73c3974965f9f25bf6644a80b52a5636
NikolaevDD <dimkanikolaev99@gmail.com>
2018-08-02 20:27:59 +0400

    slider is done

--- common.js
+++ common.js
@@ -1,29 +1,37 @@
-var line = $('.slide-line').width();
-var slide = line /(line / $('.slide-item').width());
 var slideCount = $('.slide-line').children().length;
 var slideNow = 1;
 
-$(document).ready(function(){
 
-  $(function nextSlide(){
 
-    if(slideNow > slideCount){
-      $('#slider').find('.next').click(function(){
-        $('.slide-line').css('transform','translateX(0)')
-        slideNow = 1;
-      })
-    }else{
-      $('#slider').find('.next').click(function(){
-        $('.slide-line').css('transform','translateX(-'+slide+'px)')
-        slideNow++;
-      });
-    }
-    console.log(slideNow);
+$(document).ready(function() {
+  $('.next').click(function(){
+    nextSlide();
   });
 
-  $(function prevSlide(){
-      $('#slider').find('.prev').click(function(){
-        $('.slide-line').css('transform','translateX('+slide+'px)')
-      });
+  $('.prev').click(function(){
+    prevSlide();
   });
 });
+
+function nextSlide() {
+  if (slideNow == slideCount || slideNow <= 0 || slideNow > slideCount){
+    $('.slide-line').css('transform', 'translate(0,0)');
+    slideNow = 1;
+  }else{
+    slideWidth = -$('.viewport').width() * (slideNow);
+    $('.slide-line').css('transform', 'translate('+ slideWidth +'px, 0)');
+    slideNow++;
+  }
+};
+
+function prevSlide() {
+    if (slideNow == 1 || slideNow <= 0 || slideNow > slideCount){
+      slideWidth = -$('.viewport').width() * (slideCount - 1);
+      $('.slide-line').css('transform', 'translate('+ slideWidth +'px, 0)');
+      slideNow = slideCount;
+    }else{
+      slideWidth = -$('.viewport').width() * (slideNow - 2);
+      $('.slide-line').css('transform', 'translate(' + slideWidth + 'px, 0)');
+    }
+    slideNow--;
+};
