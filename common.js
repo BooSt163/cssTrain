03a9f313ded05e37e265512849245e9c859f4894
NikolaevDD <dimkanikolaev99@gmail.com>
2018-08-06 13:12:09 +0400

    slider with tabs is done

--- common.js
+++ common.js
@@ -4,25 +4,22 @@ var tabId = 0;
 
 
 $(document).ready(function() {
-  $('.next').click(function(){
-    nextSlide();
-  });
 
-  $('.prev').click(function(){
-    prevSlide();
-  });
+  $('.arrow').click(function(){
+    var ths = $(this);
+    nav(ths);
+  })
 
   for (var i = 1; i <= slideCount; i++) { //метод, который берет порядковый номер слайда, и выводит заголовок - ссылку на него (еще нет)
     $('.tabsList').append('<li class="tabItem"></li>');
   };
-  for( var i = 0; i <= slideCount; i++){
-    slideTitle = $('.slide-item .title').text();
-    $('.tabItem').text(slideTitle);
+  for(var i = 0; i <= slideCount; i++){
+    slideTitle = $($('.slide-item .title')[i]).text();
+    $($('.tabItem')[i]).text(slideTitle);
   };
 
   $('.tabItem').click(function(){ //Навигация по табам
     tabId = $(this).index();
-
     if (tabId + 1 != slideNow){
       slideWidth = -$('.viewport').width() * (tabId);
       $('.slide-line').css('transform', 'translate('+ slideWidth +'px, 0)');
@@ -32,18 +29,17 @@ $(document).ready(function() {
 
 });
 
-function nextSlide() {
-  if (slideNow == slideCount || slideNow <= 0 || slideNow > slideCount){
-    $('.slide-line').css('transform', 'translate(0,0)');
-    slideNow = 1;
-  }else{
-    slideWidth = -$('.viewport').width() * (slideNow);
-    $('.slide-line').css('transform', 'translate('+ slideWidth +'px, 0)');
-    slideNow++;
-  }
-};
-
-function prevSlide() {
+function nav(arrow) {
+  if (arrow.hasClass('next')) {
+    if (slideNow == slideCount || slideNow <= 0 || slideNow > slideCount){
+      $('.slide-line').css('transform', 'translate(0,0)');
+      slideNow = 1;
+    }else{
+      slideWidth = -$('.viewport').width() * (slideNow);
+      $('.slide-line').css('transform', 'translate('+ slideWidth +'px, 0)');
+      slideNow++;
+    }
+  } else if (arrow.hasClass('prev')) {
     if (slideNow == 1 || slideNow <= 0 || slideNow > slideCount){
       slideWidth = -$('.viewport').width() * (slideCount - 1);
       $('.slide-line').css('transform', 'translate('+ slideWidth +'px, 0)');
@@ -53,4 +49,5 @@ function prevSlide() {
       $('.slide-line').css('transform', 'translate(' + slideWidth + 'px, 0)');
     }
     slideNow--;
-};
+  }
+}
